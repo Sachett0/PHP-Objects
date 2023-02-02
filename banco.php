53e94a0201cfb67ebd59d54897e641c1fbcfa50f
Sachetto <lucas.sachetto84@gmail.com>
2023-02-02 09:41:01 -0300

    Corrigindo arquivo banco.php

--- banco.php
+++ banco.php
@@ -2,6 +2,8 @@
 
 require_once 'autoload.php';
 
+use Alura\Banco\Modelo\Conta\ContaCorrente;
+use Alura\Banco\Modelo\Conta\ContaPoupanca;
 use Alura\Banco\Modelo\Conta\Titular;
 use Alura\Banco\Modelo\Endereco;
 use Alura\Banco\Modelo\CPF;
@@ -9,7 +11,7 @@ use Alura\Banco\Modelo\Conta\Conta;
 
 $endereco = new Endereco('Petrópolis', 'um bairro', 'minha rua', '71B');
 $vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
-$primeiraConta = new Conta($vinicius);
+$primeiraConta = new ContaCorrente($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
 
@@ -18,10 +20,10 @@ echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
 $patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $endereco);
-$segundaConta = new Conta($patricia);
+$segundaConta = new ContaPoupanca($patricia);
 var_dump($segundaConta);
 
 $outroEndereco = new Endereco('A', 'b', 'c', '1D');
-$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
+$outra = new ContaCorrente(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
